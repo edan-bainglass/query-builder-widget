9b5692a90a6433f427bed847b99301b063ae03cd
Edan Bainglass <edan.bainglass@gmail.com>
2024-08-14 14:51:38 +0200

    Add `Console` class for debugging

---
+++ src/qb_widget/utils/console.py
@@ -0,0 +1,12 @@
+import logging
+
+logger = logging.getLogger("qb_widget")
+logger.setLevel(logging.INFO)
+
+logger.handlers = [logging.FileHandler("console.log", "w")]
+
+class Console:
+
+    @staticmethod
+    def log(message):
+        logger.info(message)
